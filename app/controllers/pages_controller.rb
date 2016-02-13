d01836680aefe3decc663abbe623704071ac61d4
Raykant <pchenkens@gmail.com>
2016-02-12 17:48:10 -0800

    I GOT THE TODOLIST TO WORKbundle exec rake db:migrate:resetbundle exec rake db:migrate:reset

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
   def home
+    @todo = Todo.new();
   end
 
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,32 +4,32 @@ module PagesHelper
   require 'json'
 
   def get_welcome
-    content_tag(:div, :class=> "welcome col-md-6 col-md-offset-3") do
+    content_tag(:div, :class => "welcome col-md-6 col-md-offset-3") do
       content_tag(:h1, get_message, :id => "welcometxt") +
-      content_tag(:h1, "", :id => "timetxt") +
-      content_tag(:h1, get_todomessage, :id => "todomessage")
+          content_tag(:h1, "", :id => "timetxt") +
+          content_tag(:h1, get_todomessage, :id => "todomessage")
     end
   end
 
   def get_message
     time = Time.new()
-    if(time.hour >= 1 && time.hour < 6) then
+    if (time.hour >= 1 && time.hour < 6) then
       return "Can't sleep, Pieter?"
     end
 
-    if(time.hour >= 5 && time.hour < 12) then
+    if (time.hour >= 5 && time.hour < 12) then
       return "Good morning, Pieter."
     end
 
-    if(time.hour >= 12 && time.hour < 17 ) then
+    if (time.hour >= 12 && time.hour < 17) then
       return "Good afternoon, Pieter."
     end
 
-    if(time.hour >= 17 && time.hour < 22) then
+    if (time.hour >= 17 && time.hour < 22) then
       return "Welcome back, Pieter."
     end
 
-    if(time.hour >= 22) then
+    if (time.hour >= 22) then
       return "Getting tired, Pieter?"
     end
   end
@@ -55,10 +55,10 @@ module PagesHelper
 
     content_tag(:div, :class => "weather col-md-2 col-md-offset-1") do
       content_tag(:i, nil, :class => get_icon(data)) +
-      content_tag(:h3, get_sunrise(data)) +
-      content_tag(:h3, get_temp(data)) +
-      content_tag(:h3, get_main(data)) +
-      content_tag(:h3, get_sunset(data))
+          content_tag(:h3, get_sunrise(data)) +
+          content_tag(:h3, get_temp(data)) +
+          content_tag(:h3, get_main(data)) +
+          content_tag(:h3, get_sunset(data))
     end
   end
 
@@ -129,11 +129,19 @@ module PagesHelper
   end
 
   def get_todo
-
-    content_tag(:div, :class => "todo col-md-4 col-md-offset-1") do
-      get_todobody
+    content_tag(:ul, :class => "todolist") do
+      content_tag(:ul, :class => "todolist") do
+        Todo.all.each do |todo|
+          concat(
+              content_tag(:li) do
+                content_tag(:h3, :class => "todoitem") do
+                  link_to(todo.item, "todos/#{todo.id}", :class => "itemlink", :method => :delete)
+                end
+              end
+          )
+        end
+      end
     end
-
   end
 
   def get_calendar
@@ -143,35 +151,29 @@ module PagesHelper
   end
 
   def get_todomessage
-    "What do you need to do?"
-  end
-
-  def get_todobody
-    content_tag(:input, "", :type => "text", :class => "text-line", :placeholder => get_exampleitem) +
-    content_tag(:ul, :class => "todolist") do
-      content_tag(:li, content_tag(:h3, "Sample", :class => "todotext"), :class => "todoitem")
+    if Todo.count > 0 then
+      return "Here's what I have for you:"
     end
+    "What do you need to do?"
   end
 
   def get_exampleitem
     num = rand(5)
-
     return case num
-              when 0
-                "Read chapter 1 of ..."
-              when 1
-                "Finish Ch. 7 Prob ..."
-              when 2
-                "Write paragraph on ..."
-              when 3
-                "Finish programming ..."
-              when 4
-                "Model part for ..."
-            end
+             when 0
+               "Read chapter 1 of ..."
+             when 1
+               "Finish Ch. 7 Prob ..."
+             when 2
+               "Write paragraph on ..."
+             when 3
+               "Finish programming ..."
+             when 4
+               "Model part for ..."
+           end
   end
 
   def get_calendarmessage
     "You have no plans"
   end
-
 end
